2fc95b27b06c52ca3bb3eecf34a47e86993a634e
MuFtHy_InShAf <136156185+Mufthy-Inshaf@users.noreply.github.com>
2023-10-14 19:01:20 +0530

    Add files via upload

---
+++ QV-Social.py
@@ -0,0 +1,112 @@
+def prRed(skk): print("\033[91m {}\033[00m" .format(skk))
+
+def prGreen(skk): print("\033[92m {}\033[00m" .format(skk))
+
+def prYellow(skk): print("\033[93m {}\033[00m" .format(skk))
+
+def prLightPurple(skk): print("\033[94m {}\033[00m" .format(skk))
+
+def prPurple(skk): print("\033[95m {}\033[00m" .format(skk))
+
+def prCyan(skk): print("\033[96m {}\033[00m" .format(skk))
+
+def prLightGray(skk): print("\033[97m {}\033[00m" .format(skk))
+
+def prBlack(skk): print("\033[98m {}\033[00m" .format(skk))
+
+#tool name
+prGreen("+---------------------------------------------------------+")
+prRed("       ____  _  __     _____            _       _ ")
+prRed("      / __ \| |/ /    / ____|          (_)     | | ")
+prRed("     | |  | | ' /____| (___   ___   ___ _  __ _| | ")
+prRed("     | |  | |  <______\___ \ / _ \ / __| |/ _` | | ")
+prRed("     | |__| | . \     ____) | (_) | (__| | (_| | | ")
+prRed("      \___\_\_|\_\   |_____/ \___/ \___|_|\__,_|_| ")
+print("")
+prGreen("+---------------------------------------------------------+")                                               
+prYellow("              Dev: @_.inshaf._.mufthy._ & v.1")                    
+prGreen("+---------------------------------------------------------+")
+
+
+
+print("")
+print("")
+
+import webbrowser as wb
+
+#q = int(input("What want to you: "))
+
+
+print("         (1) Instagram", "                  (11) pinterest" )		
+print("         (2) Facebook","                   (12) Tumblr")		 
+print("         (3) Whatsapp","                   (13) Flickr")
+print("         (4) Google","                     (14) Viber")
+print("         (5) Telegram","                   (15) Snapchat")
+print("         (6) Tiktok","                     (16) Quora")	
+print("         (7) Skype","                      (17) Vimeo")	
+print("         (8) Linkedin","                   (18) Bizsugar")
+print("         (9) Twitter","                    (19) Mix")	
+print("        (10) Youtube","                    (20) Webchat")	
+
+print("")				
+												
+i = int(input("Enter your social media number: "))
+				
+					
+if i == 1 :							
+	#import webbrowser as wb
+	wb.open("https://www.instagram.com/")
+if i == 2:
+	wb.open("https://www.facebook.com/")
+if i == 3:
+	wb.open("https://www.whatsapp.com/")
+if i == 4:
+	wb.open("https://www.google.com/")
+if i == 5:
+	wb.open("https://www.telegram.org/")
+if i == 6:
+	wb.open("https://www.tiktok.com/en/")
+if i == 7:
+	wb.open("https://www.skype.com/en/" )
+if i == 8:
+	wb.open("https://www.linkedin.com/ ")
+if i == 9:
+	wb.open("https://twitter.com/")
+if i == 10:
+	wb.open("https://www.youtube.com/")
+if i == 11:
+	wb.open("https://www.zoom.us/")
+if i == 12:
+	wb.open("https://www.pinterest.com/")
+if i == 13:
+	wb.open("https://www.tumblr.com/")
+if i == 14:
+	wb.open("https://www.flickr.com/")
+if i == 15:
+	wb.open("http://www.viber.com/en/")
+if i == 16:
+	wb.open("https://www.snapchat.com/")
+if i == 17:
+	wb.open("https://www.quora.com/")
+if i == 18:
+	wb.open("https://vimeo.com/")
+if i == 19:
+	wb.open("http://www.bizsugar.com/")
+if i == 20:
+	wb.open("https://mix.com/")
+if i == 21:
+	wb.open("http://digg.com/")
+if i == 22:
+	wb.open("https://www.wechat.com/en/")
+if i == 23:
+	wb.open("https://medium.com/")
+
+if i == 24:
+	wb.open("http://192.168.43.160:8080")
+#if i == 24:
+#	wb.open("")
+#if i == 25:
+#	wb.open("")
+
+
+input( )
